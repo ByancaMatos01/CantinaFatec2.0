d83abb473a376e02ad3cca8ce38026fff81ad60a
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2024-10-28 14:57:18 -0300

    feat: save reserved itens on an array

--- script.js
+++ script.js
@@ -1,60 +1,92 @@
 // Definindo a quantidade e os preços dos itens
 const itemQuantities = {
-    burger: 10,
-    coxinha: 5,
-    quibe: 16,
-    refri: 30,
-    bolo: 20,
-    pizza: 15,
-    sorvete: 25,
-    cupcake: 3  
+  burger: 10,
+  coxinha: 5,
+  quibe: 16,
+  refri: 30,
+  bolo: 20,
+  pizza: 15,
+  sorvete: 25,
+  cupcake: 3,
 };
 
 const itemPrices = {
-    burger: 12,
-    coxinha: 6,
-    quibe: 8,
-    refri: 10,
-    bolo: 18,
-    pizza: 20,
-    sorvete: 5,
-    cupcake: 8
+  burger: 10,
+  coxinha: 8,
+  quibe: 8,
+  refri: 8,
+  bolo: 15,
+  pizza: 30,
+  sorvete: 12,
+  cupcake: 18,
 };
 
+const itemNames = {
+  burger: "Burger",
+  coxinha: "Coxinha",
+  quibe: "Quibe",
+  refri: "Refrigerante",
+  bolo: "Bolo",
+  pizza: "Pizza",
+  sorvete: "Sorvete",
+  cupcake: "Cupcake",
+};
+
+let cart = [];
+
 // Inicialize o total do pedido com o valor armazenado no localStorage ou zero se não houver
-let totalPedido = parseFloat(localStorage.getItem('totalPedido')) || 0;
+let totalPedido = parseFloat(localStorage.getItem("totalPedido")) || 0;
 
 function reserveItem(item) {
-    // Verifica se o item ainda possui estoque
-    if (itemQuantities[item] > 0) {
-        // Diminui a quantidade em 1
-        itemQuantities[item]--;
+  // Verifica se o item ainda possui estoque
+  if (itemQuantities[item] > 0) {
+    // Diminui a quantidade em 1
+    itemQuantities[item]--;
 
-        // Atualiza a quantidade exibida
-        document.getElementById(`${item}-qty`).querySelector('span').textContent = itemQuantities[item];
+    // Atualiza a quantidade exibida
+    document.getElementById(`${item}-qty`).querySelector("span").textContent =
+      itemQuantities[item];
 
-        // Verifica se a quantidade chegou a zero para desabilitar o botão
-        if (itemQuantities[item] === 0) {
-            const button = document.getElementById(`${item}-btn`);
-            button.disabled = true;
-            button.textContent = 'Indisponível';
-            button.style.backgroundColor = 'red';
-            button.style.color = 'white';
-        }
-
-        // Adiciona o preço do item ao total do pedido
-        totalPedido += itemPrices[item];
-        localStorage.setItem('totalPedido', totalPedido.toFixed(2)); // Armazena o total no localStorage
-
-        // Pergunta ao usuário se deseja continuar reservando ou finalizar
-        const continuar = confirm(`Você reservou um(a) ${item}. Deseja continuar reservando?`);
-
-        // Verifica a resposta do usuário
-        if (!continuar) {
-            // Se o usuário escolher finalizar, redireciona para a página de cadastro
-            window.location.href = 'cadastro.html';
-        }
-    } else {
-        alert(`Desculpe, ${item} está indisponível.`);
+    // Verifica se a quantidade chegou a zero para desabilitar o botão
+    if (itemQuantities[item] === 0) {
+      const button = document.getElementById(`${item}-btn`);
+      button.disabled = true;
+      button.textContent = "Indisponível";
+      button.style.backgroundColor = "red";
+      button.style.color = "white";
     }
+
+    // Adiciona o preço do item ao total do pedido
+    totalPedido += itemPrices[item];
+    localStorage.setItem("totalPedido", totalPedido.toFixed(2)); // Armazena o total no localStorage
+
+    // Adicionar o item ao array de pedidos
+    let cartItem = cart.find((c) => c.id === item);
+    if (cartItem) {
+      cartItem.quantity++;
+      cartItem.price += itemPrices[item];
+    } else {
+      cart.push({
+        id: item,
+        name: itemNames[item],
+        quantity: 1,
+        price: itemPrices[item],
+      });
+    }
+
+    console.log(cart);
+
+    // Pergunta ao usuário se deseja continuar reservando ou finalizar
+    const continuar = confirm(
+      `Você reservou um(a) ${item}. Deseja continuar reservando?`
+    );
+
+    // Verifica a resposta do usuário
+    if (!continuar) {
+      // Se o usuário escolher finalizar, redireciona para a página de cadastro
+      window.location.href = "carrinho.html";
+    }
+  } else {
+    alert(`Desculpe, ${item} está indisponível.`);
+  }
 }
