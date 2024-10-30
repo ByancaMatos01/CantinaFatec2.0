29de79c8bed9a98c231dc43fd13671faebeaaeda
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2024-10-30 15:02:25 -0300

    refactor: capitalize all consts

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Definindo a quantidade e os preços dos itens
-const itemQuantities = {
+const ITEM_QUANTITIES = {
   burger: 10,
   coxinha: 5,
   quibe: 16,
@@ -10,7 +10,7 @@ const itemQuantities = {
   cupcake: 3,
 };
 
-const itemPrices = {
+const ITEM_PRICES = {
   burger: 10,
   coxinha: 8,
   quibe: 8,
@@ -21,7 +21,7 @@ const itemPrices = {
   cupcake: 18,
 };
 
-const itemNames = {
+const ITEM_NAMES = {
   burger: "Burger",
   coxinha: "Coxinha",
   quibe: "Quibe",
@@ -39,16 +39,16 @@ let totalPedido = parseFloat(localStorage.getItem("totalPedido")) || 0;
 
 function reserveItem(item) {
   // Verifica se o item ainda possui estoque
-  if (itemQuantities[item] > 0) {
+  if (ITEM_QUANTITIES[item] > 0) {
     // Diminui a quantidade em 1
-    itemQuantities[item]--;
+    ITEM_QUANTITIES[item]--;
 
     // Atualiza a quantidade exibida
     document.getElementById(`${item}-qty`).querySelector("span").textContent =
-      itemQuantities[item];
+      ITEM_QUANTITIES[item];
 
     // Verifica se a quantidade chegou a zero para desabilitar o botão
-    if (itemQuantities[item] === 0) {
+    if (ITEM_QUANTITIES[item] === 0) {
       const button = document.getElementById(`${item}-btn`);
       button.disabled = true;
       button.textContent = "Indisponível";
@@ -57,25 +57,24 @@ function reserveItem(item) {
     }
 
     // Adiciona o preço do item ao total do pedido
-    totalPedido += itemPrices[item];
+    totalPedido += ITEM_PRICES[item];
     localStorage.setItem("totalPedido", totalPedido.toFixed(2)); // Armazena o total no localStorage
 
     // Adicionar o item ao array de pedidos
     let cartItem = cart.find((c) => c.id === item);
     if (cartItem) {
       cartItem.quantity++;
-      cartItem.price += itemPrices[item];
+      cartItem.price += ITEM_PRICES[item];
     } else {
       cart.push({
         id: item,
-        name: itemNames[item],
+        name: ITEM_NAMES[item],
         quantity: 1,
-        price: itemPrices[item],
+        price: ITEM_PRICES[item],
       });
     }
 
     localStorage.setItem("cart", JSON.stringify(cart));
-    console.log(localStorage.getItem("cart"));
 
     // Pergunta ao usuário se deseja continuar reservando ou finalizar
     const continuar = confirm(
@@ -121,7 +120,7 @@ function renderCart() {
     const cartItem = document.createElement("tr");
     cartItem.innerHTML = `
       <td colspan="2">${item.name}</td>
-                        <td>$${itemPrices[item.id].toFixed(2)}</td>
+                        <td>$${ITEM_PRICES[item.id].toFixed(2)}</td>
                         <td>
                             <button class="icon-btn"><i class="fa-solid fa-minus"></i></button>
                             ${item.quantity}
